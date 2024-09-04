f8749cdbecd7abdbb45303bb1412b714aa51532e
Ari Lamstein <arilamstein@gmail.com>
2024-09-04 11:04:58 -0700

    fix comment

--- base-r.R
+++ base-r.R
@@ -1,7 +1,7 @@
-### Use Base R to:
-1. Read in a CSV file of US Counties to a dataframe.
-2. Subset the rows in the dataframe to counties named `Washington`.
-3. Subset the columns in the dataframe to `county.name` and `state.name`.
+# Use Base R to:
+# 1. Read in a CSV file of US Counties to a dataframe.
+# 2. Subset the rows in the dataframe to counties named `Washington`.
+# 3. Subset the columns in the dataframe to `county.name` and `state.name`.
 
 df = read.csv('county_data.csv')
 df[df$county.name == "washington", c("county.name", "state.name")]
